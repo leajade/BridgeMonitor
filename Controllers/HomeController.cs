2f0dfbf317a1d7a27498da3f378b6e77bb37a67b
Lea Duvigneau <lea.duvigneau@ynov.com>
2021-04-01 10:03:21 +0200

    add page Details

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace BridgeMonitor.Controllers
             return View();
         }
 
+        public IActionResult Details()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
