1b39dafa55963d4ae208d20cd00e04a066c18992
Lea Duvigneau <lea.duvigneau@ynov.com>
2021-04-01 09:59:09 +0200

    add pages index, clothings

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace BridgeMonitor.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Clothings()
         {
             return View();
         }
